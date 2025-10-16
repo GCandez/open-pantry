2c092eac1d23724dfa9d6564e761b7fe648de906
Gabriel <gabrielcandez@gmail.com>
2025-10-16 17:48:17 -0300

    feat: add `PreviousEntityStateNullException` for null entity state handling
    - Introduced `PreviousEntityStateNullException<TAction>` sealed class in `OpenPantry.Domain.Common.Entities`
    - Provides specialized exception for scenarios involving null entity states during action applications

---
+++ Source/OpenPantry.Domain/Common/Entities/PreviousEntityStateNullException.cs
@@ -0,0 +1,3 @@
+﻿namespace OpenPantry.Domain.Common.Entities;
+
+public sealed class PreviousEntityStateNullException<TAction>() : Exception($"Cannot apply {typeof(TAction).Name} to a null entity");
