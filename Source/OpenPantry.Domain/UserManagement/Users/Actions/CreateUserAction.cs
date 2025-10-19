e0b89f6f44c9db89e51aec5bee0daa2731849d7a
Gabriel <gabrielcandez@gmail.com>
2025-10-19 20:32:16 -0300

    feat: add CreateUserAction and UserCreatedEvent
    - Introduced `CreateUserAction` to encapsulate user creation logic with event generation
    - Added `UserCreatedEvent` to represent user creation events, including metadata and state application logic
    - Enhanced domain model with explicit handling for user creation actions and corresponding events

---
+++ Source/OpenPantry.Domain/UserManagement/Users/Actions/CreateUserAction.cs
@@ -0,0 +1,34 @@
+﻿using OpenPantry.Domain.Common.Entities;
+using OpenPantry.Domain.UserManagement.Users.Events;
+using OpenPantry.Domain.UserManagement.Users.ValueObjects;
+
+namespace OpenPantry.Domain.UserManagement.Users.Actions;
+
+public sealed class CreateUserAction : EntityAction<UserEntity, UserCreatedEvent>
+{
+    public required UserName Name { get; init; }
+
+    protected override (UserEntity NewState, UserCreatedEvent ResultingEvent) DeriveNewState(UserEntity? previousState)
+    {
+        var user = new UserEntity
+        {
+            Id = Guid.CreateVersion7(Timestamp),
+            Name = Name,
+            CreatedAt = Timestamp,
+            UpdatedAt = Timestamp
+        };
+
+        var @event = new UserCreatedEvent
+        {
+            Id = Guid.CreateVersion7(Timestamp),
+            Timestamp = Timestamp,
+            Data = new()
+            {
+                UserId = user.Id,
+                UserName = user.Name.Value
+            }
+        };
+
+        return (user, @event);
+    }
+}
---
+++ Source/OpenPantry.Domain/UserManagement/Users/Events/UserCreatedEvent.cs
@@ -0,0 +1,24 @@
+﻿using OpenPantry.Domain.Common.Entities;
+using OpenPantry.Domain.UserManagement.Users.ValueObjects;
+
+namespace OpenPantry.Domain.UserManagement.Users.Events;
+
+public sealed record UserCreatedEvent : EntityEvent<UserEntity, UserCreatedEvent.EventData>
+{
+    public sealed record EventData
+    {
+        public required Guid UserId { get; init; }
+        public required string UserName { get; init; }
+    }
+
+    public override UserEntity Apply(UserEntity? previousState)
+    {
+        return new()
+        {
+            Id = Data.UserId,
+            Name = UserName.From(Data.UserName),
+            CreatedAt = Timestamp,
+            UpdatedAt = Timestamp
+        };
+    }
+}
