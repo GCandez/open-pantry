543220ec17d90dbf96227830c1764fd7aefcf67d
Gabriel <gabrielcandez@gmail.com>
2025-10-19 20:32:03 -0300

    feat: add `UserEntity` and `UserName` value object
    - Introduced `UserEntity` record to represent user entities in the domain model
    - Added `UserName` value object with validation for proper encapsulation and constraints
    - Enhanced domain consistency with strongly typed user-related entities and value objects

---
+++ Source/OpenPantry.Domain/UserManagement/Users/UserEntity.cs
@@ -0,0 +1,9 @@
+﻿using OpenPantry.Domain.Common.Entities;
+using OpenPantry.Domain.UserManagement.Users.ValueObjects;
+
+namespace OpenPantry.Domain.UserManagement.Users;
+
+public sealed record UserEntity : Entity<UserEntity>
+{
+    public required UserName Name { get; init; }
+}
---
+++ Source/OpenPantry.Domain/UserManagement/Users/ValueObjects/UserName.cs
@@ -0,0 +1,66 @@
+﻿using OpenPantry.Domain.Common.Results;
+using OpenPantry.Domain.Common.ValueObjects;
+
+namespace OpenPantry.Domain.UserManagement.Users.ValueObjects;
+
+public readonly struct UserName : IValueObject<UserName>
+{
+    public bool Equals(UserName other)
+    {
+        return Value == other.Value;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is UserName other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return Value.GetHashCode();
+    }
+
+    public static bool operator ==(UserName left, UserName right)
+    {
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(UserName left, UserName right)
+    {
+        return !left.Equals(right);
+    }
+
+    public readonly string Value;
+
+    private UserName(string value)
+    {
+        Value = value;
+    }
+
+    public enum ValidationError
+    {
+        TooShort,
+        TooLong
+    }
+
+    public static Result<UserName, ValidationError> Create(string value)
+    {
+        value = value.Trim();
+
+        return value.Length switch
+        {
+            < 3 => ValidationError.TooShort,
+            > 20 => ValidationError.TooLong,
+            _ => new UserName(value)
+        };
+    }
+
+    public static UserName From(string value)
+    {
+        var result = Create(value);
+
+        return result.IsFailure
+            ? throw new ValueObjectValidationException<UserName, ValidationError>(result.Error)
+            : result.Value;
+    }
+}
