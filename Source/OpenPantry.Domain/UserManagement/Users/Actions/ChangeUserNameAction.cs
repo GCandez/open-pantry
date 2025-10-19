efef3eae9069b820d179e789cb29bac36454e843
Gabriel <gabrielcandez@gmail.com>
2025-10-19 20:32:24 -0300

    feat: add ChangeUserNameAction and UserNameChangedEvent
    - Introduced `ChangeUserNameAction` to encapsulate logic for changing user names in the domain model
    - Added `UserNameChangedEvent` to represent changes in user names with metadata and state application logic
    - Enhanced domain model with explicit handling for name change actions and corresponding events

---
+++ Source/OpenPantry.Domain/UserManagement/Users/Actions/ChangeUserNameAction.cs
@@ -0,0 +1,43 @@
+﻿using OpenPantry.Domain.Common.Entities;
+using OpenPantry.Domain.Common.Results;
+using OpenPantry.Domain.UserManagement.Users.Events;
+using OpenPantry.Domain.UserManagement.Users.ValueObjects;
+
+namespace OpenPantry.Domain.UserManagement.Users.Actions;
+
+public sealed class ChangeUserNameAction : EntityAction<UserEntity, ChangeUserNameAction.Error, UserNameChangedEvent>
+{
+    public required UserName NewName { get; init; }
+
+    public enum Error
+    {
+        SameName
+    }
+
+    protected override Result<(UserEntity NewState, UserNameChangedEvent ResultingEvent), Error> DeriveNewState(UserEntity? previousState)
+    {
+        if (previousState is null)
+            throw new PreviousEntityStateNullException<ChangeUserNameAction>();
+
+        if (previousState.Name == NewName)
+            return Error.SameName;
+
+        var newState = previousState with
+        {
+            Name = NewName,
+            UpdatedAt = Timestamp
+        };
+
+        var @event = new UserNameChangedEvent
+        {
+            Id = Guid.CreateVersion7(Timestamp),
+            Timestamp = Timestamp,
+            Data = new()
+            {
+                NewUserName = newState.Name.Value
+            }
+        };
+
+        return (newState, @event);
+    }
+}
---
+++ Source/OpenPantry.Domain/UserManagement/Users/Events/UserNameChangedEvent.cs
@@ -0,0 +1,24 @@
+﻿using OpenPantry.Domain.Common.Entities;
+using OpenPantry.Domain.UserManagement.Users.ValueObjects;
+
+namespace OpenPantry.Domain.UserManagement.Users.Events;
+
+public sealed record UserNameChangedEvent : EntityEvent<UserEntity, UserNameChangedEvent.EventData>
+{
+    public sealed record EventData
+    {
+        public required string NewUserName { get; init; }
+    }
+
+    public override UserEntity Apply(UserEntity? previousState)
+    {
+        if (previousState is null)
+            throw new InvalidOperationException("Cannot apply UserNameChangedEvent to a null UserEntity");
+
+        return previousState with
+        {
+            Name = UserName.From(Data.NewUserName),
+            UpdatedAt = Timestamp
+        };
+    }
+}
